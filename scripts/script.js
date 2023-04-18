646e265410a3b552f371bfa85fb0194ad3bdd12c
Gabriel Nunes <gaseinesdzn@hotmail.com>
2023-04-18 10:57:01 -0300

    Atualização de animação

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
+function mudouTamano(){
+    var home = document.getElementById('casa')
+    var menu = document.getElementById('imenu')
+    var sectionRedes = document.getElementById('home')
+    var ciclos = document.getElementById('ciclos')
+    if(window.innerWidth >= 600){
+        menu.style.width = '100%'
+        menu.style.height = '50px'
+        home.style.display = 'inline'
+        ciclos.style.display = 'inline'
+    }else{
+        menu.style.padding = '0px 0px'
+        menu.style.height = '0px'
+        home.style.display = 'none'
+        ciclos.style.display = 'none'
+        
+    }
+}
+
 function Menu(){
     var home = document.getElementById('casa')
     var menu = document.getElementById('imenu')
@@ -11,8 +30,10 @@ function Menu(){
         ciclos.style.display = 'none'
         sectionRedes.style.width = '80%'
         
-    }else{
+    }
+    else{
         menu.style.padding = '20px 0px'
+        menu.style.width = '100px'
         menu.style.height = '100%'
         home.style.display = 'block'
         ciclos.style.display = 'block'
